090cded9e9268ab5f92f635f3691e6402f025a5f
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2024-05-30 23:16:02 -0500

    Update slickback.py

--- slickback.py
+++ slickback.py
@@ -2,11 +2,13 @@
 #"simple" is for purely linear networks without batching or activation functions.
 #I also recommend for your first layer in any 2d network you do x = torch.fft.fftshift(torch.fft.ifft2(x).real) + x
 #What is slickback? 
-#it is an improved optimizer(relative to adam) which accelerates convergence
+#it is an improved optimizer(relative to adam/sgd) which accelerates convergence through identifying overshoot and incremental progression
+#https://www.youtube.com/watch?v=47jKGHZfplY
+#if you're using SGD for a simple linear network with no gates, try simple mode. it can be faster than sgd
 
 class Slickback:
     #https://github.com/falseywinchnet/AI_STUFF
-    def __init__(self, params, lr=0.001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, simple=False):
+   def __init__(self, params, lr=0.001, betas=(0.9, 0.999), eps=1e-8, weight_decay=0, simple=False):
         self.params = list(params)
         self.lr = lr
         self.betas = betas
@@ -28,8 +30,15 @@ class Slickback:
 
     def simple_step(self):
         for i, param in enumerate(self.params):
-            if param.grad is not None:
-                grad = param.grad.data
+                if param.grad is not None:
+                  stx = torch.std(param.grad).item()
+                  while stx < 0.1:
+                    stx = stx * 10
+                  sty = torch.norm(param.grad).item()
+                  if stx>sty:
+                    grad = param.grad * (stx **0.1)
+                  else:
+                    grad = param.grad/(sty/2)
 
                 if self.grad_history[param] is not None:
                     sign_change = (self.grad_history[param] * grad).lt(0)
