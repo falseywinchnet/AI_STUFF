a8a3b8468536ee0cc817f215441a80de8ef8f41f
falseywinchnet <87070787+falseywinchnet@users.noreply.github.com>
2024-11-05 12:36:47 -0500

    Update RCO.py

--- RCO.py
+++ RCO.py
@@ -8,10 +8,10 @@ from torch.optim import Optimizer
 class RCO_new(Optimizer):
     """
       Runge-Kutta-Chebyshev Optimizer (RCO) - A neural network optimizer that combines
-      4th order Runge-Kutta method and Chebyshev polynomial interpolation for simulated annealing
+      4th order polynomial interpolation on two scales for simulated annealing
 
       This optimizer implements a novel approach to neural network optimization by:
-      1. Computing gradients using both RK4 and Chebyshev methods
+      1. Computing gradients using both RK4 and a vaguely Chebyshev method
       2. Averaging the results of both methods
       3. Applying the combined update to the model parameters
 
@@ -46,9 +46,9 @@ class RCO_new(Optimizer):
       >>> for epoch in range(num_epochs):
       ...     loss = optimizer.step(x_batch, y_batch)
      """
-    def __init__(self, model, dt=18):
+    def __init__(self, model):
             self.model = model
-            self.dt = dt
+            self.dt = 1
 
     def compute_loss(self, x, y):
         y_pred = self.model(x)
@@ -66,7 +66,7 @@ class RCO_new(Optimizer):
       orig_params = [p.data.clone() for p in self.model.parameters()]
       
       for p, k in zip(self.model.parameters(), k1):
-          p.data -= k * ( self.dt/2)
+          p.data -= k * 2
       self.backpropagate(x,y)
       k2_rk4 = [p.grad.clone() for p in self.model.parameters()]
 
@@ -74,7 +74,7 @@ class RCO_new(Optimizer):
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
       for p, k in zip(self.model.parameters(), k2_rk4):
-          p.data -= k *  (self.dt/3)
+          p.data -= k *  4
       self.backpropagate(x,y)
       k3_rk4 = [p.grad.clone() for p in self.model.parameters()]
 
@@ -82,7 +82,7 @@ class RCO_new(Optimizer):
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
       for p, k in zip(self.model.parameters(), k3_rk4):
-          p.data -= k *  self.dt
+          p.data -= k *  12
       self.backpropagate(x,y)
       k4_rk4 = [p.grad.clone() for p in self.model.parameters()]
 
@@ -90,16 +90,12 @@ class RCO_new(Optimizer):
       # Reset for Chebyshev
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
-
-      # Chebyshev nodes
-      c1 = (self.dt * 1/11)
-      c2 =  (self.dt *  2/11)
-      c3 =  (self.dt *  3/11)
-      c4 =  (self.dt *   4/11)
+       
+      # Pre-computed optimal weights
 
       # k2 Cheb
       for p, k in zip(self.model.parameters(), k1):
-          p.data -= k * c1
+          p.data -= k *2
       self.backpropagate(x,y)
       k2_cheb = [p.grad.clone() for p in self.model.parameters()]
 
@@ -107,7 +103,7 @@ class RCO_new(Optimizer):
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
       for p, k in zip(self.model.parameters(), k2_cheb):
-          p.data -= k * c2
+          p.data -= k * 3
       self.backpropagate(x,y)
       k3_cheb = [p.grad.clone() for p in self.model.parameters()]
 
@@ -115,7 +111,7 @@ class RCO_new(Optimizer):
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
       for p, k in zip(self.model.parameters(), k3_cheb):
-          p.data -= k * c3
+          p.data -= k * 4
       self.backpropagate(x,y)
       k4_cheb = [p.grad.clone() for p in self.model.parameters()]
 
@@ -123,35 +119,29 @@ class RCO_new(Optimizer):
       for p, orig in zip(self.model.parameters(), orig_params):
           p.data.copy_(orig)
       for p, k in zip(self.model.parameters(), k4_cheb):
-          p.data -= k * c4
+          p.data -= k * 5
       self.backpropagate(x,y)
       k5_cheb = [p.grad.clone() for p in self.model.parameters()]
 
       # Combine RK4  result
       rk4_update = []
       for k1_p, k2_p, k3_p, k4_p in zip(k1, k2_rk4, k3_rk4, k4_rk4):
-          update = ((k1_p +  ( self.dt/( self.dt/2))*k2_p +( self.dt/( self.dt/2))*k3_p + k4_p)/( self.dt/2))
+          update = (k1_p +  2*k2_p +2*k3_p + k4_p)/6
           rk4_update.append(update)
 
-      #print the abs of the sum of rk4_update
-      # Combine Cheb result
-      w1 = 0.21967452022181430116
-      w2 = 0.23876183658069730087
-      w3 = 0.25950763224531042672
-      w4 = 0.28205601095217797125
-
       cb4_update = []
-      for k1_p, k2_p, k3_p, k4_p, k5_p in zip(k1, k2_cheb, k3_cheb, k4_cheb, k5_cheb):
-          cb4_update = (w1*k2_p + w2*k3_p + w3*k4_p + w4*k5_p)
+      for k1_p, k2_p, k3_p, k4_p in zip(k2_cheb, k3_cheb, k4_cheb,k5_cheb):
+          cb4_update = ( 0.4*k1_p + 0.3*k2_p + 0.2*k3_p +0.1*k4_p)
 
     
       for i, (p, rk4_u, cheb_u) in enumerate(zip(self.model.parameters(), rk4_update, cb4_update)):
-          update = (rk4_u  - cheb_u)/2
+          update = (rk4_u  + cheb_u)/2
           p.data += update
 
       model.zero_grad()
       final_loss = self.compute_loss(x, y)
       return final_loss.item()
+
 
 
 
